71513ff1b070899f8d3115e8fd88232c08a817ad
unknown <karanrana3095@gmail.com>
2022-06-28 11:40:00 +0530

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function Square({value}) {// value props-props
+                          // destructing
+  return (
+    <button type="button">{value}
+    </button>
+  )
+}
+export default Square;
